8726074f48eae450cf69ee0347216daa4465d1f1
tanmay-tiwari-20 <tanmaytiwari200905@gmail.com>
2024-07-05 20:28:09 +0530

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-var tl = gsap.timeline()
+function loadingAnimation(){
+    var tl = gsap.timeline()
 tl.from(".line h1", {
     y: 150,
     stagger: 0.25,
@@ -33,9 +34,29 @@ tl.from("#page1", {
     y:1600,
     opacity:0,
     delay:0.2,
-    duration:0.5,
+    duration:0.6,
     ease:Power4
 })
 tl.to("#loader",{
     display:"none"
 })
+tl.from("nav",{
+    opacity:0
+})
+tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1",{
+    y:120,
+    stagger:0.2
+})
+}
+function cursorAnimation(){
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y,
+        })
+    })
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation()
+cursorAnimation()
