9a4548810c5ea707b62be0e6854e3bb51c0a80ef
tanmay-tiwari-20 <tanmaytiwari200905@gmail.com>
2024-07-06 19:42:45 +0530

    animations were added

--- script.js
+++ script.js
@@ -85,15 +85,42 @@ tl.from("#hero1,#page2",{
 },"-=1.2")
 }
 function cursorAnimation(){
-    document.addEventListener("mousemove",function(dets){
-        gsap.to("#crsr",{
-            left:dets.x,
-            top:dets.y,
+    Shery.mouseFollower({
+        skew:true,
+        ease:"cubic-bezier(0.23.1,0.320,1)",
+        duration:1,
+    })
+    Shery.makeMagnet("#nav-part2 h4");
+    var videoContainer = document.querySelector("#video-container")
+    videoContainer.addEventListener("mouseenter",function(){
+        videoContainer.addEventListener("mousemove", function(dets){
+            gsap.to(".mousefollower",{
+                opacity:0
+            })
+            gsap.to("#video-cursor",{
+                left:dets.x - 500,
+                y:dets.y - 200
+            })
         })
     })
-    
-    Shery.makeMagnet("#nav-part2 h4");
+    videoContainer.addEventListener("mouseleave",function(){
+        gsap.to(".mousefollower",{
+            opacity:1
+        })
+        gsap.to("#video-cursor",{
+            left:"70%",
+            top:"-18%"
+        })
+    })
 }
+function sheryAnimation(){
+    Shery.imageEffect(".image-div",{
+        style:5,
+        config:{"a":{"value":1.6,"range":[0,30]},"b":{"value":-1,"range":[-1,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.7241466211046166},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":true},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.61,"range":[0,10]},"metaball":{"value":0.49,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":6.87,"range":[0,100]}},
+        gooey:true
+    })
+}
+cursorAnimation()
 locomotiveAnimation()
 loadingAnimation()
-// cursorAnimation()
+sheryAnimation()
